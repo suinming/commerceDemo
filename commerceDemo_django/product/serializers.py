220adb2b736e108dc19549b76dfd62288e8c732c
suinming <suinming@laserlab04-PC.localdomain>
2022-07-15 16:09:16 +0800

    build POST api

--- commerceDemo_django/product/serializers.py
+++ commerceDemo_django/product/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Product
+from .models import Product, Category
 
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -13,4 +13,17 @@ class ProductSerializer(serializers.ModelSerializer):
             "price",
             "get_image",
             "get_thumbnail"
+        )
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    products = ProductSerializer(many=True)
+
+    class Meta:
+        model = Category
+        fields = (
+            "id",
+            "name",
+            "get_absolute_url",
+            "products",
         )
--- commerceDemo_django/product/urls.py
+++ commerceDemo_django/product/urls.py
@@ -4,5 +4,10 @@ from product import views
 
 urlpatterns = [
   path('latest-products/', views.LatestProductsList.as_view()),
-  path('products/<slug:category_slug>/<slug:product_slug>/', views.ProductDetail.as_view()),
+  path('products/<slug:category_slug>/<slug:product_slug>/',
+       views.ProductDetail.as_view()),
+  path('products/<slug:category_slug>/', views.CategoryDetail.as_view()),
+  # do not add '/'  at the end of the url in search href
+  # if you add '/' it will cause the 405 error due to CORS
+  path('products/search', views.search)
 ]
